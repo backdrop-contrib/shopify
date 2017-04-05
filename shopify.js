53ca9687f2cc73525f24df4256d7f696941d20a4
Daniel Pepin <donutdan4114@gmail.com>
2017-04-05 13:12:51 -0400

    Update shopify cart js to be outside the block and in the js file.

--- shopify.js
+++ shopify.js
@@ -27,11 +27,31 @@
     });
   };
 
+  /**
+   * Displays the cart quantity.
+   */
+  Drupal.shopify.updateCartQuantity = function () {
+    var url = $('#shopify-cart-link').attr('href');
+    $.ajax({
+      type: 'GET',
+      url: url + '.json',
+      dataType: 'jsonp',
+      success: function (data) {
+        var total = 0;
+        for (i = 0; i < data.items.length; i++) {
+          total += data.items[i].quantity;
+        }
+        $('#shopify-cart-total').text(total);
+      }
+    });
+  };
+
   Drupal.behaviors.shopify = {
     attach: function (context, settings) {
       Drupal.shopify.ctx = context;
       Drupal.shopify.settings = settings;
       Drupal.shopify.display_add_to_cart_message();
+      Drupal.shopify.updateCartQuantity();
     }
   }
 
